24e40f748f3df305313c623c8e2263abbf0414f1
test 01 - Nguyen Cao Lan <rubby@yopmail.com>
2026-09-10 15:51:32 +0700

    feat/cloud-kingdom-2

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,14 @@
+const powerUp = "star";
+let effect= '';
+if (powerUp === "mushroom"){
+    effect = "Mario becomes Super!";
+}else if (powerUp === "flower"){
+    effect = "Mario can shoot fireballs!";
+}else if (powerUp === "star"){
+    effect = "Mario is invincible!";
+}else if (powerUp === "none") {
+    effect = "Mario is normal";
+}else {
+    effect = "Unknown power-up";
+}
+console.log (effect);
